47eb376f1b8119066f577c068c0c694c1a0ba94b
konglomora <arsenii.netrebenko@gmail.com>
2021-06-07 00:25:23 +0300

    ✒ REFACTORING: Output data

--- Homework/02_Cycles&Branching/script.js
+++ Homework/02_Cycles&Branching/script.js
@@ -1,7 +1,6 @@
 let N = prompt('Enter first integer number', 0); // Запрашиваем первое число
 
 while ( N.trim() === '' || isNaN(+N) || !Number.isInteger(+N) ) {// Валидируем
-    alert(typeof N);
     N = prompt('It`s not a integer number, please try again', 0); // Запрашиваем первое число пока валидация не пройдена
 }
 
@@ -16,12 +15,11 @@ let sumOfnumbers = 0;
 let minNumber = Math.min(+N, +M); // Определяем минимальное знаечение из N и M
 let maxNumber = Math.max(+N, +M); // Определяем максимальное знаечение из N и M
 
-for (minNumber; minNumber <= maxNumber; minNumber++) {
-    if (skipParity && minNumber % 2 == 0) {
-        // Проверяем пропускать парные или нет
+for (let i = minNumber; i <= maxNumber; i++) {
+    if (skipParity && i % 2 == 0) {// Проверяем пропускать парные или нет
         continue;
     }
-    sumOfnumbers += minNumber; // Складываем сумму всех чисел в промежутке
+    sumOfnumbers += i; // Складываем сумму всех чисел в промежутке
 }
 
-alert(`Sum of numbers is ${sumOfnumbers}`);
+alert(`Sum of numbers from ${minNumber} to ${maxNumber} is ${sumOfnumbers}`);
