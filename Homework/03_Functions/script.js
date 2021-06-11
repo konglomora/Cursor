01df87396d71280f669ddfc49453388970bf7386
konglomora <arsenii.netrebenko@gmail.com>
2021-06-11 18:34:28 +0300

    👌 IMPROVE: Layout

--- Homework/03_Functions/script.js
+++ Homework/03_Functions/script.js
@@ -13,8 +13,13 @@ function getMaxDigit(number) {
     number = String(number); // Переводим число в строку, что б оперировать её методами
     let digits = [];
 
-    for (let i = 0; i < number.length; i++) // Пока итератор меньше длинны строки
-        if (/[\d]/.test(number[i])) {  // Если символ с индексом которому равен итератор это еденичное число 
+    for (
+        let i = 0;
+        i < number.length;
+        i++ // Пока итератор меньше длинны строки
+    )
+        if (/[\d]/.test(number[i])) {
+            // Если символ с индексом которому равен итератор это еденичное число
             digits.push(number[i]); // Пушим его в массив
         }
     const maxDigit = Math.max(...digits); // Получаем максимальное число из созданного массива благодаря spread
@@ -51,7 +56,7 @@ function getAmountAfterTax(salarySum) {
     const taxPercent = 19.5;
     const taxSum = salarySum * (taxPercent / 100);
     const amountAfterTax = salarySum - taxSum;
-    return `Salary sum after tax ${taxPercent}%  = ${amountAfterTax} UAH`;
+    return `${amountAfterTax.toFixed(2)} UAH`;
 }
 
 // * Task 5
@@ -144,41 +149,139 @@ function deleteDuplicateLetter(str) {
     return uniqueLettersString;
 }
 
-document.querySelector('.functions--block').innerHTML = `
+// ! HTML -----------------------------------------------------------------------------------------------
+// * Task 1 ---------------------------------------------------------------------------------------------
 
-<span class="functions__item">Функція №1 getMaxDigit(-128123.9): ${getMaxDigit(
-    '-128123.9'
-)}</span>
-<span class="functions__item">Функція №2 getNumPower(2, 10): ${getNumPower(
-    2,
-    10
-)}</span>
-<span class="functions__item">Функція №3 capitalizeFirstletter(WhAt iS lovE?): ${capitalizeFirstletter(
-    'WhAt iS lovE?'
-)}</span>
-<span class="functions__item">Функція №4 getAmountAfterTax(10000): ${getAmountAfterTax(
-    10000
-)}</span>
-<span class="functions__item">Функція №5 getRandomNumber(2, 20): ${getRandomNumber(
-    2,
-    20
-)}</span>
-<span class="functions__item">Функція №6 countLetter('a', 'Astalavista'): ${countLetter(
-    'a',
-    'Astalavista'
-)}</span>
-<span class="functions__item">Функція №8 getRandomPassword(15): ${getRandomPassword(
-    15
-)}</span>
-<span class="functions__item">Функція №9 deleteLetters('s', 'Solar chrest'): ${deleteLetters(
-    's',
-    'Solar chrest'
-)}</span>
-<span class="functions__item">Функція №10 isPalyndrom('Я несу гусеня'): ${isPalyndrom(
-    'Я несу гусеня'
-)}</span>
-<span class="functions__item">Функція №11 deleteDuplicateLetter('Praise the sun'): ${deleteDuplicateLetter(
-    'Praise the sun'
-)}</span>
+function runFirstTask() {
+    const number = document.querySelector('.function1__input').value;
+    document.querySelector(
+        '.function1__result'
+    ).innerHTML = `<span class="result">Результат: ${getMaxDigit(
+        number
+    )}</span>`;
+}
 
-`;
+document.querySelector('.function1__submit').onclick = runFirstTask;
+
+// * Task 2 ---------------------------------------------------------------------------------------------
+
+function runSecondTask() {
+    const num = document.querySelector('.function2__input1').value;
+    const pow = document.querySelector('.function2__input2').value;
+    document.querySelector(
+        '.function2__result'
+    ).innerHTML = `<span class="result">Результат: ${getNumPower(
+        num,
+        pow
+    )}</span>`;
+}
+
+document.querySelector('.function2__submit').onclick = runSecondTask;
+
+// * Task 3 ---------------------------------------------------------------------------------------------
+
+function runThirdTask() {
+    const str = document.querySelector('.function3__input').value;
+    document.querySelector(
+        '.function3__result'
+    ).innerHTML = `<span class="result">Результат: ${capitalizeFirstletter(
+        str
+    )}</span>`;
+}
+
+document.querySelector('.function3__submit').onclick = runThirdTask;
+
+// * Task 4 ---------------------------------------------------------------------------------------------
+
+function runFourthTask() {
+    const number = document.querySelector('.function4__input').value;
+    document.querySelector(
+        '.function4__result'
+    ).innerHTML = `<span class="result">Результат: ${getAmountAfterTax(
+        number
+    )}</span>`;
+}
+
+document.querySelector('.function4__submit').onclick = runFourthTask;
+
+// * Task 5 ---------------------------------------------------------------------------------------------
+
+function runFifthTask() {
+    const startNumber = document.querySelector('.function5__input1').value;
+    const endNumber = document.querySelector('.function5__input2').value;
+    document.querySelector(
+        '.function5__result'
+    ).innerHTML = `<span class="result">Результат:  ${getRandomNumber(
+        startNumber,
+        endNumber
+    )}</span>`;
+}
+
+document.querySelector('.function5__submit').onclick = runFifthTask;
+
+// * Task 6 ---------------------------------------------------------------------------------------------
+
+function runSixthTask() {
+    const letter = document.querySelector('.function6__input1').value;
+    const str = document.querySelector('.function6__input2').value;
+    document.querySelector(
+        '.function6__result'
+    ).innerHTML = `<span class="result">Результат:  ${countLetter(
+        letter,
+        str
+    )}</span>`;
+}
+
+document.querySelector('.function6__submit').onclick = runSixthTask;
+
+// * Task 8 ---------------------------------------------------------------------------------------------
+
+function runEighthTask() {
+    const n = document.querySelector('.function8__input').value;
+    document.querySelector(
+        '.function8__result'
+    ).innerHTML = `<span class="result">Результат: ${getRandomPassword(
+        n
+    )}</span>`;
+}
+
+document.querySelector('.function8__submit').onclick = runEighthTask;
+
+// * Task 9 ---------------------------------------------------------------------------------------------
+
+function runNinthTask() {
+    const letter = document.querySelector('.function9__input1').value;
+    const str = document.querySelector('.function9__input2').value;
+    document.querySelector(
+        '.function9__result'
+    ).innerHTML = `<span class="result">Результат: ${deleteLetters(
+        letter,
+        str
+    )}</span>`;
+}
+
+document.querySelector('.function9__submit').onclick = runNinthTask;
+
+// * Task 10 ---------------------------------------------------------------------------------------------
+
+function runTenthTask() {
+    const str = document.querySelector('.function10__input').value;
+    document.querySelector(
+        '.function10__result'
+    ).innerHTML = `<span class="result">Результат: ${isPalyndrom(str)}</span>`;
+}
+
+document.querySelector('.function10__submit').onclick = runTenthTask;
+
+// * Task 11---------------------------------------------------------------------------------------------
+
+function runEleventhTask() {
+    const str = document.querySelector('.function11__input').value;
+    document.querySelector(
+        '.function11__result'
+    ).innerHTML = `<span class="result">Результат: ${deleteDuplicateLetter(
+        str
+    )}</span>`;
+}
+
+document.querySelector('.function11__submit').onclick = runEleventhTask;
