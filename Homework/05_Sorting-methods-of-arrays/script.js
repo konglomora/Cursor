39382a5970a62f46a1e32a93635ff6e5d98ca254
konglomora <arsenii.netrebenko@gmail.com>
2021-06-18 23:08:04 +0300

    📦 NEW: Tasks and functions

--- Homework/05_Sorting-methods-of-arrays/script.js
+++ Homework/05_Sorting-methods-of-arrays/script.js
@@ -21,15 +21,22 @@ function skipNotInteger(array) {
 
 function getRandomArray(length, min, max) {
     if (
-        !Number.isInteger(length) ||
-        !Number.isInteger(min) ||
-        !Number.isInteger(max)
+        isNotaNumber(length) ||
+        isNotaNumber(min) ||
+        isNotaNumber(max) ||
+        !Number.isInteger(+length) ||
+        !Number.isInteger(+min) ||
+        !Number.isInteger(+max)
     ) {
         return 'Please, use integer!';
     }
+    minNumber = Math.min(min, max);
+    maxNumber = Math.max(min, max);
     const randomArray = [];
     for (let i = 0; i < length; i++) {
-        const randomNumber = Math.trunc(min + Math.random() * (max - min));
+        const randomNumber = Math.trunc(
+            minNumber + Math.random() * (maxNumber - minNumber)
+        );
         randomArray.push(randomNumber);
     }
     return randomArray;
@@ -41,29 +48,40 @@ function getRandomArray(length, min, max) {
 // ? Створіть функцію getModa(...numbers) – яка вираховує моду всіх переданих в неї аргументів.
 // ! НЕЦІЛІ ЧИСЛА ІГНОРУЮТЬСЯ
 // ** Приклад: getModa(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2) –> 2
-// TODO: Support for multiple mod values
 
-function getModa(...args) {
-    if (!args.length || args.some(isNaN)) return 'Please, try again!';
-    const intNumbers = skipNotInteger(args);
-    let modaCount = 0; // Счётчик количества повторений значения моды
-    let repetitionCounter = 0; // Счетчик повторений каждого значения массива
-    let modaValue = intNumbers[0]; // Значение моды
-    for (let i = 0; i < intNumbers.length; i++) {
-        if (repetitionCounter > modaCount) {
-            modaCount = repetitionCounter;
-            modaValue = intNumbers[i - 1];
+const getModa = (...args) => {
+    // Валидируем
+    if (!args.length) return 'You have not entered anything';
+    else if (args.some(isNotaNumber))
+        return 'You entered not a number or did not use a comma';
+
+    const intNumbers = skipNotInteger(args).sort((a, b) => a - b); // Пропускаем нецелые числа и сортируем
+    let repetitionCounter = 1;
+    const repetitionValues = [];
+
+    intNumbers.forEach((number, i) => {
+        if (number === intNumbers[++i]) {
+            repetitionCounter++; // Если элементы рядом и они равны друг другу, то увеличиваем счётчик
+        } else repetitionCounter = 1; // А если рядом с элементом нет равных ему значений - значит он такой 1
+        repetitionValues.push(repetitionCounter);
+    });
+
+    const maxRepetition = Math.max(...repetitionValues); // Получаем значения максимальных повторений
+    const modaArray = [];
+
+    repetitionValues.forEach((repetitonCount, i) => {
+        if (repetitonCount === maxRepetition) {
+            // Если значение повторения максимальное, то можно пушить в массив элемент который соответствует этому повторению
+            modaArray.push(intNumbers[i]);
         }
-        repetitionCounter = 0;
-        for (let j = i; j < intNumbers.length; j++) {
-            if (intNumbers[j] == intNumbers[i]) repetitionCounter++;
-        }
-    }
+    });
+    if (modaArray.length === 1) return `Moda value = ${String(modaArray)}`;
+    // Если значение моды это 1 число
+    else if (modaArray.length > 1)
+        return `Moda values = ${String(modaArray.join(' and '))}`; // Если значений моды несколько
+};
 
-    return `Moda value = ${modaValue}`;
-}
-
-// console.log(getModa(1, 1, 1, 2, 2, 2));
+// console.log(getModa()); // [ 6, 2]
 
 // * Task 3
 // ? Створіть функцію getAverage(...numbers) – яка рахує середнє арифметичне всіх переданих в неї аргументів.
@@ -72,12 +90,12 @@ function getModa(...args) {
 
 function getAverage(...numbers) {
     if (!numbers.length) return 'You have not entered anything';
-    else if (numbers.some(isNaN)) return 'You entered not a number';
+    else if (numbers.some(isNotaNumber))
+        return 'You entered not a number or did not use a comma';
+
     const intNumbers = skipNotInteger(numbers);
     return intNumbers.reduce((a, b) => a + b) / intNumbers.length;
 }
-
-// console.log(getAverage(1.2, 2.1,4, 2, 1, 3, 3.2, '11'));
 
 // * Task 4
 // ? Створіть функцію getMedian(...numbers) – яка рахує медіану всіх переданих в неї аргументів.
@@ -87,6 +105,10 @@ function getAverage(...numbers) {
 // ** Приклад: getMedian(1, 2, 3, 4, 5) –> 3
 
 function getMedian(...numbers) {
+    if (!numbers.length) return 'You have not entered anything';
+    else if (numbers.some(isNotaNumber))
+        return 'You entered not a number or did not use a comma';
+
     const intNumbers = skipNotInteger(numbers);
     const half = Math.floor(intNumbers.length / 2);
     intNumbers.sort((a, b) => a - b);
@@ -98,47 +120,44 @@ function getMedian(...numbers) {
     }
 }
 
-// console.log(getMedian(1, 2 ,3 , 4 , 5, 6, 7, 12.2));
-
 // * Task 5
 // ? Створіть функцію filterEvenNumbers(...numbers) – яка фільтрує парні числа передані як аргументи функції
 // ** Приклад: filterEvenNumbers(1, 2, 3, 4, 5, 6) -> [1, 3, 5]
 
 function filterEvenNumbers(...numbers) {
+    if (!numbers.length) return 'You have not entered anything';
+    else if (numbers.some(isNotaNumber))
+        return 'You entered not a number or did not use a comma';
+
     return numbers.filter((number) => number % 2);
 }
-// console.log(`Even numbers = ${filterEvenNumber(1, 2, 3, 4, 5, 6)}`); // 1, 3, 5
 
 // * Task 6
 // ?  Створіть функцію countPositiveNumbers(...numbers) – яка порахує кількість чисел більших 0
 // ** Приклад: countPositiveNumbers(1, -2, 3, -4, -5, 6) -> 3
 
 function countPositiveNumbers(...numbers) {
-    if (numbers.some(isNotaNumber)) {
-        return 'Use number, please';
-    }
+    if (!numbers.length) return 'You have not entered anything';
+    else if (numbers.some(isNotaNumber))
+        return 'You entered not a number or did not use a comma';
+
     return numbers.reduce((count, number) => {
         if (number > 0) count++;
         return count;
     }, 0);
 }
 
-// console.log(countPositiveNumbers(1, -2, 3, -5, 6)); // 3
-
 // * Task 7
 // ? Створіть функцію getDividedByFive(...numbers) – яка відфільтрує усі елементи в масиві та залишить тільки ті, які діляться на ціло на 5
 // ** Приклад: getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2) -> [55, 55]
 
 function getDividedByFive(...numbers) {
-    if (numbers.some(isNotaNumber)) {
-        return 'Use number, please';
-    }
+    if (!numbers.length) return 'You have not entered anything';
+    else if (numbers.some(isNotaNumber))
+        return 'You entered not a number or did not use a comma';
+
     return numbers.filter((number) => number % 5 === 0);
 }
-
-// console.log(
-//     getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2) // [ 55, 55 ]
-// );
 
 // * Task 8
 // ? Створіть функцію replaceBadWords(string) – яка 1) розіб'є фразу на слова, 2) замінить погані слова на зірочки (*).
@@ -179,7 +198,7 @@ function replaceBadWords(str, ...swearing) {
     return stringWords.join(' ');
 }
 
-console.log(replaceBadWords('funck fucking shit with bitches', 'bitch'));
+// console.log(replaceBadWords('funck fucking shit with bitches', 'bitch'));
 
 // * Task 9
 // ? Створіть функцію divideByThree(word), яка розбиває кожне слово на умовні склади по 3 букви.
@@ -188,13 +207,16 @@ console.log(replaceBadWords('funck fucking shit with bitches', 'bitch'));
 // ** divideByThree("Commander) -> ["com", "man", "der"]
 // ** Приклад: divideByThree("live") -> ["liv", "e"]
 
-function divideByThree(word) {
+function divideBySyllables(word, numberOfSyllables) {
     if (!word.length) return 'Please, input something';
+    else if (isNotaNumber(numberOfSyllables))
+        return 'Use number for number of syllables';
+
     let wordsArr = word.toLowerCase().replaceAll(' ', '');
     let result = [];
-    if (wordsArr.length > 3) {
-        for (let i = 0; i < wordsArr.length; i += 3) {
-            result.push(wordsArr.slice(i, i + 3));
+    if (wordsArr.length > numberOfSyllables) {
+        for (let i = 0; i < wordsArr.length; i += numberOfSyllables) {
+            result.push(wordsArr.slice(i, i + numberOfSyllables));
         }
     } else {
         return wordsArr;
@@ -202,4 +224,167 @@ function divideByThree(word) {
     return result;
 }
 
-// console.log(divideByThree('live')); // ["cas", "do"]
+// ! HTML -----------------------------------------------------------------------------------------------
+
+// * Task 1 ---------------------------------------------------------------------------------------------
+
+function runFirstTask() {
+    const length = document.querySelector('.function1__input1').value;
+    const min = document.querySelector('.function1__input2').value;
+    const max = document.querySelector('.function1__input3').value;
+    document.querySelector(
+        '.function1__result'
+    ).innerHTML = `<span class="result">Результат: ${getRandomArray(
+        length,
+        min,
+        max
+    )}</span>`;
+}
+
+document.querySelector('.function1__submit').onclick = runFirstTask;
+
+// * Task 2 ---------------------------------------------------------------------------------------------
+
+function runSecondTask() {
+    const stringArr = document
+        .querySelector('.function2__input1')
+        .value.split(',');
+    const numbersArray = stringArr.map((item, i, arr) => {
+        if (arr[i].trim() === '') {
+            arr.splice(--i, 1);
+        } else return +item;
+    });
+    document.querySelector(
+        '.function2__result'
+    ).innerHTML = `<span class="result">Результат: ${getModa(
+        ...numbersArray
+    )}</span>`;
+}
+
+document.querySelector('.function2__submit').onclick = runSecondTask;
+
+// * Task 3 ---------------------------------------------------------------------------------------------
+
+function runThirdTask() {
+    const stringArr = document
+        .querySelector('.function3__input')
+        .value.split(',');
+    const numbersArray = stringArr.map((item, i, arr) => {
+        if (arr[i].trim() === '') {
+            arr.splice(--i, 1);
+        } else return +item;
+    });
+    document.querySelector(
+        '.function3__result'
+    ).innerHTML = `<span class="result">Результат: ${getAverage(
+        ...numbersArray
+    )}</span>`;
+}
+
+document.querySelector('.function3__submit').onclick = runThirdTask;
+
+// * Task 4 ---------------------------------------------------------------------------------------------
+
+function runFourthTask() {
+    const stringArr = document
+        .querySelector('.function4__input')
+        .value.split(',');
+
+    const numbersArray = stringArr.map((item, i, arr) => {
+        if (arr[i].trim() === '') {
+            arr.splice(--i, 1);
+        } else return +item;
+    });
+    document.querySelector(
+        '.function4__result'
+    ).innerHTML = `<span class="result">Результат: ${getMedian(
+        ...numbersArray
+    )}</span>`;
+}
+
+document.querySelector('.function4__submit').onclick = runFourthTask;
+
+// * Task 5 ---------------------------------------------------------------------------------------------
+
+function runFifthTask() {
+    const stringArr = document
+        .querySelector('.function5__input')
+        .value.split(',');
+
+    const numbersArray = stringArr.map((item, i, arr) => {
+        if (arr[i].trim() === '') {
+            arr.splice(--i, 1);
+        } else return +item;
+    });
+    document.querySelector(
+        '.function5__result'
+    ).innerHTML = `<span class="result">Результат: ${filterEvenNumbers(
+        ...numbersArray
+    )}</span>`;
+}
+
+document.querySelector('.function5__submit').onclick = runFifthTask;
+
+// * Task 6 ---------------------------------------------------------------------------------------------
+
+function runSixthTask() {
+    const stringArr = document
+        .querySelector('.function6__input')
+        .value.split(',');
+
+    const numbersArray = stringArr.map((item, i, arr) => {
+        if (arr[i].trim() === '') {
+            arr.splice(--i, 1);
+        } else return +item;
+    });
+    document.querySelector(
+        '.function6__result'
+    ).innerHTML = `<span class="result">Результат:  ${countPositiveNumbers(
+        ...numbersArray
+    )}</span>`;
+}
+
+document.querySelector('.function6__submit').onclick = runSixthTask;
+
+// * Task 8 ---------------------------------------------------------------------------------------------
+
+function runSeventhTask() {
+    const string = document.querySelector('.function8__input1').value;
+    const otherBadWords = document.querySelector('.function8__input2').value;
+    document.querySelector(
+        '.function8__result'
+    ).innerHTML = `<div class="result">Результат: ${replaceBadWords(
+        string,
+        ...otherBadWords
+    )}</div>`;
+}
+
+document.querySelector('.function8__submit').onclick = runSeventhTask;
+
+// * Task 8 ---------------------------------------------------------------------------------------------
+
+function runNinthTask() {
+    const letter = document.querySelector('.function9__input1').value;
+    const str = document.querySelector('.function9__input2').value;
+    document.querySelector(
+        '.function9__result'
+    ).innerHTML = `<span class="result">Результат: ${deleteLetters(
+        letter,
+        str
+    )}</span>`;
+}
+
+document.querySelector('.function9__submit').onclick = runNinthTask;
+
+// * Task 9 ---------------------------------------------------------------------------------------------
+
+function runTenthTask() {
+    const str = document.querySelector('.function10__input').value;
+    document.querySelector(
+        '.function10__result'
+    ).innerHTML = `<span class="result">Результат: ${isPalyndrom(str)}</span>`;
+}
+
+document.querySelector('.function10__submit').onclick = runTenthTask;
+
+
