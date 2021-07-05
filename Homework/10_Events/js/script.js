137d7133663294d1ce2a82a031da553eeeaac988
Arsenii Netrebenko <boecrejha@gmail.com>
2021-07-06 01:27:49 +0300

    👌 IMPROVE: Refactoring

--- Homework/10_Events/js/script.js
+++ Homework/10_Events/js/script.js
@@ -1,18 +1,18 @@
- function playSound(element){
+function playSound(element) {
   const audio = document.querySelector(`audio[data-key="${element.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${element.keyCode}"]`);
   if (!audio) return;
   audio.currentTime = 0;
   audio.play();
-  key.classList.add('down')
+  key.classList.add('down');
 }
 
-function removeTransition(element){
+function removeTransition(element) {
   if (element.propertyName !== 'transform') return;
-  this.classList.remove('down')
+  this.classList.remove('down');
 }
 
 const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+keys.forEach((key) => key.addEventListener('transitionend', removeTransition));
 
-window.addEventListener('keydown', playSound)
+window.addEventListener('keydown', playSound);
