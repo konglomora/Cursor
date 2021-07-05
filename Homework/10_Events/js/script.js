a4d113f33933d6f1e8b7b28b5a9f7ab8094ec62c
Arsenii Netrebenko <boecrejha@gmail.com>
2021-07-06 01:21:42 +0300

    👌 IMPROVE: Functions work

--- Homework/10_Events/js/script.js
+++ Homework/10_Events/js/script.js
@@ -1,6 +1,18 @@
-window.addEventListener('keydown', function(e){
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+ function playSound(element){
+  const audio = document.querySelector(`audio[data-key="${element.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${element.keyCode}"]`);
   if (!audio) return;
+  audio.currentTime = 0;
   audio.play();
-  console.log(audio);
-})
+  key.classList.add('down')
+}
+
+function removeTransition(element){
+  if (element.propertyName !== 'transform') return;
+  this.classList.remove('down')
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+
+window.addEventListener('keydown', playSound)
