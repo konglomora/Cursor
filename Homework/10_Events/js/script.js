2c62af429443c7af943d9f12305952b8e352c03e
Arsenii Netrebenko <boecrejha@gmail.com>
2021-07-06 13:34:17 +0300

    📦 NEW: Click function

--- Homework/10_Events/js/script.js
+++ Homework/10_Events/js/script.js
@@ -1,10 +1,11 @@
-function playSound(element) {
-  const audio = document.querySelector(`audio[data-key="${element.keyCode}"]`);
-  const key = document.querySelector(`.key[data-key="${element.keyCode}"]`);
+function playSound(event) {
+  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
   if (!audio) return;
   audio.currentTime = 0;
   audio.play();
   key.classList.add('down');
+  console.log(key);
 }
 
 function removeTransition(element) {
@@ -13,6 +14,17 @@ function removeTransition(element) {
 }
 
 const keys = document.querySelectorAll('.key');
-keys.forEach((key) => key.addEventListener('transitionend', removeTransition));
+
+keys.forEach((key) => {
+  key.onclick = function () {
+    const audio = document.querySelector(
+      `audio[data-key="${key.dataset.key}"]`
+    );
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('down');
+  };
+  key.addEventListener('transitionend', removeTransition);
+});
 
 window.addEventListener('keydown', playSound);
