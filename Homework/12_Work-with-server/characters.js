de85cfb57d2e052a8cfb23d976f5822a777a090f
Arsenii Netrebenko <boecrejha@gmail.com>
2021-07-17 00:05:47 +0300

    📦 NEW: Function for planets render

--- Homework/12_Work-with-server/characters.js
+++ Homework/12_Work-with-server/characters.js
@@ -1,19 +1,15 @@
-const resultsBlock = document.querySelector('.results');
+const charactersBlock = document.querySelector('.characters');
 
 const filmsURL = 'https://swapi.dev/api/films/';
 
-
-
 async function getCharatersInfo(url, id) {
-  const res = await fetch(url + '/' + id);
-  const episode = await res.json();
-  // output += `<div class="episode-title">${episode.title}</div>`;
+  const response = await fetch(url + '/' + id);
+  const episode = await response.json();
   console.log(episode.title);
   await displayCharacters(episode);
 }
 
 async function displayCharacters(episode) {
- 
   let output = '';
   episode.characters.forEach(characterUrl =>
     fetch(characterUrl).then(response => {
@@ -34,19 +30,18 @@ async function displayCharacters(episode) {
           if (gender === 'male') genderSrc = `./img/gender_icons/male.svg`;
           if (gender === 'female') genderSrc = `./img/gender_icons/female.svg`;
           if (gender === 'hermaphrodite')
-            genderSrc = `./img/gender_icons/hermaphrodite.png`;
+            genderSrc = `./img/gender_icons/hermaphrodite.svg`;
           output += `
-          <div class="characters__block">
-          <div class="chatacter__name">${name}</div>
+          <div class="characters__card">
+          <div class="character__name">${name}</div>
           <img class="character__photo--image" src="${chracterPhotoSRC}" alt="Character photo">
-          <div class="chatacter__birth-year">Birth year: ${birthYear}</div>
-          <div class="chatacter__birth-year">Gender:</div>
+          <div class="character__birth-year">Birth year: ${birthYear}</div>
           <img class="character__gender--image" src="${genderSrc}" alt="Character gender">
 
           </div>
           `;
         })
-        .then(result => (resultsBlock.innerHTML = output));
+        .then(() => (charactersBlock.innerHTML = output));
     })
   );
 }
@@ -57,4 +52,3 @@ const selectEpisodes = document.getElementById('episodes-select');
 getCharactersButton.addEventListener('click', () => {
   getCharatersInfo(filmsURL, selectEpisodes.value);
 });
-
---
+++ Homework/12_Work-with-server/planets.js
@@ -0,0 +1,26 @@
+const getPlanetsButton = document.getElementById('getPlanets');
+
+getPlanetsButton.addEventListener('click', () => {
+  getPlanets();
+});
+
+const planetsBlock = document.querySelector('.planets');
+
+async function getPlanets() {
+  let planetsItem = '';
+
+  const response = await fetch('https://swapi.dev/api/planets/');
+  const planets = await response.json();
+  console.log(planets.results);
+  planets.results.forEach(planet => {
+    const planetName = planet.name;
+    let planetImageSRC = `./img/planets/${planetName.replaceAll(' ', '_')}.jpg`;
+    planetsItem += `
+    <div class="planet__card">
+        <div class="planet-name">${planetName}</div>
+        <img class="planet-image" src="${planetImageSRC}" alt="Character photo">
+    </div>
+    `;
+  });
+  planetsBlock.innerHTML = planetsItem;
+}
