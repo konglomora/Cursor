496d5a58a0fd7acd2411690425e58bdddf2aa8ff
Arsenii Netrebenko <boecrejha@gmail.com>
2021-07-27 02:15:22 +0300

    👌 IMPROVE: Storage

--- Homework/15_Tools--Code-modularity-and-Webpack/src/js/image.js
+++
@@ -1,6 +0,0 @@
-// import image from './assets/images/webpack.png'
-
-// const img = document.createElement('img');
-// img.setAttribute('src', image)
-// img.classList.add('logo')
-// document.body.appendChild(img)
